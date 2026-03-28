6571bbe252010ed208a2e86595dba611f50f3371
Santiago Castillo <santiagogabrielcastillo@hotmail.com>
2026-03-28 17:05:48 -0300

    feat(routes): nest manual_trades under exchange_accounts

--- config/routes.rb
+++ config/routes.rb
@@ -18,6 +18,7 @@ Rails.application.routes.draw do
   resources :exchange_accounts, only: %i[index new create destroy edit update] do
     member do
       post :sync
+      resources :manual_trades, only: %i[new create edit update destroy]
     end
   end
   resources :trades, only: :index
