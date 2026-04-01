0ded336e675f8b7b11b3815f339e487a724d64f9
Santiago Castillo <santiagogabrielcastillo@hotmail.com>
2026-04-01 07:56:12 -0300

    fix: wrap dashboard threads in with_connection, ensure both threads joined on error

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -9,13 +9,26 @@ class DashboardsController < ApplicationController
       nil
     end
 
-    summary_thread    = Thread.new { Dashboards::SummaryService.call(current_user, mep_rate: mep_rate) }
-    allocation_thread = Thread.new { Allocations::SummaryService.call(user: current_user, mep_rate: mep_rate) }
+    summary_thread    = Thread.new do
+      ActiveRecord::Base.connection_pool.with_connection do
+        Dashboards::SummaryService.call(current_user)
+      end
+    end
+    allocation_thread = Thread.new do
+      ActiveRecord::Base.connection_pool.with_connection do
+        Allocations::SummaryService.call(user: current_user, mep_rate: mep_rate)
+      end
+    end
 
-    result = summary_thread.value
-    result[:summary_trades_path] = trades_path(result[:summary_trades_path_params])
-    @dashboard = Struct.new(*result.keys, keyword_init: true).new(**result)
+    begin
+      result = summary_thread.value
+      result[:summary_trades_path] = trades_path(result[:summary_trades_path_params])
+      @dashboard = Struct.new(*result.keys, keyword_init: true).new(**result)
 
-    @allocation_summary = allocation_thread.value
+      @allocation_summary = allocation_thread.value
+    ensure
+      # Guarantee both threads are joined and their connections returned even if one raises.
+      [summary_thread, allocation_thread].each { |t| t.join rescue nil }
+    end
   end
 end
