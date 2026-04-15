1d4b6cc91fe4b4ffdc1d484afb35eb4442152f22
Santiago Castillo <santiagogabrielcastillo@hotmail.com>
2026-04-15 19:11:18 -0300

    fix(js): clear turbo-frame src attribute on modal close to prevent stale reload

--- app/javascript/controllers/turbo_modal_controller.js
+++ app/javascript/controllers/turbo_modal_controller.js
@@ -14,6 +14,7 @@ export default class extends Controller {
       this.dialogTarget.close()
     }
     if (this.hasFrameTarget) {
+      this.frameTarget.removeAttribute("src")
       this.frameTarget.innerHTML = ""
     }
   }
