d25ccefdb5374a8408ed0673c82b468f4e852421
Santiago Castillo <santiagogabrielcastillo@hotmail.com>
2026-04-20 17:07:20 -0300

    chore: update queue_schema.rb after positions_data migration

--- db/queue_schema.rb
+++ db/queue_schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_04_18_104844) do
+ActiveRecord::Schema[8.1].define(version: 2026_04_20_194534) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -365,6 +365,7 @@ ActiveRecord::Schema[8.1].define(version: 2026_04_18_104844) do
   create_table "stock_portfolio_snapshots", force: :cascade do |t|
     t.decimal "cash_flow", precision: 16, scale: 2, default: "0.0", null: false
     t.datetime "created_at", null: false
+    t.jsonb "positions_data", default: [], null: false
     t.datetime "recorded_at", null: false
     t.string "source", default: "manual", null: false
     t.bigint "stock_portfolio_id", null: false
